02c87fca3fb7532e4c4bc7fcf9d3f8083b997e09
osillyscope <brandoncb95@gmail.com>
2017-01-16 02:10:13 -0500

    Finished the hex conversion function.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "printf.hpp"
 
 int main(){
-    int len = printf("foo %%\nThis is a string: %s\nThis is the biggest 64-bit int: ", "Woo string!", 9223372036854775807);
+    printf("%x\n", 4560);
 }
+
