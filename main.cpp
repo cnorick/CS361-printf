732e43f613ebc81b2a5855368d697555820a1200
cnorick <cnathanorick@gmail.com>
2017-01-16 13:31:52 -0500

    Revert "Start %f implementation"
    This reverts commit 82991499434f75b160801e96fb15eb9d13d484a9.

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main(){
     int64_t bar = -15;
     char testString[20] = "Woo String!!";
 
+
     printf("%x\n", 5678);
     printf("Biggest int in hex: %x\n", biggestInt);
     printf("Biggest uint in hex: %x\n", biggestUInt);
@@ -17,7 +18,6 @@ int main(){
     printf("-1 in hex: %x\n", -1);
     printf("foo %%\nThis is a string: %s\n", testString);
     printf("These are ints: %d, %d\nThis is the biggest 64-bit int: %d\nThis is the smallest int: %d\nThis is zero: %d\n", foo, bar, biggestInt, smallestInt, 0);
-    printf("%f\n", 10.123);
 
 }
 
